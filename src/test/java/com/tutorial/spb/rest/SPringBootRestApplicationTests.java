df7f906e4580a4ad7f731c5ec8496bffc8e9f660
BezuhaniciLaura <bezuhanici.laura@gmail.com>
2021-03-18 11:08:04 +0200

    Initial commit

---
+++ src/test/java/com/tutorial/spb/rest/SPringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.spb.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SPringBootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
